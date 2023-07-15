9fa73acd0131ec17b3cedf6510f07424bd61fcab
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2023-07-15 19:43:33 -0400

    adding snipeit adapter

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express"
-
+import {assetRouter} from "./Routes/Assets"
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use("/assets", assetRouter)
 
 app.listen(2000, () => {
     console.log(`backend listening on port 2000`)
